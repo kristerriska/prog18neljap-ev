fccc96cd2a6e65fb628fc510838b362aac36d497
if17 <if17@A406-10.INFORMAATIKA>
2018-02-01 10:17:45 +0200

    Toatemperatuur

--- Algus/src/algus/Algus.java
+++ Algus/src/algus/Algus.java
@@ -10,7 +10,17 @@ package algus;
  * @author provisional
  */
 public class Algus {
-
+    
+    /**
+     * Alamprogramm toa soojuse hindamiseks vastavalt temperatuurile.
+     * vähemalt kolm taset
+     */
+    public static void toatemperatuur(int temperatuur){      
+        if(temperatuur<18){System.out.println("Külm");}
+        if(temperatuur>30){System.out.println("Palav");}
+        if(30>temperatuur>18){System.out.println("Paras");}
+    }
+    
     /**
      * Arvamus ilma kohta 
      */
@@ -18,15 +28,18 @@ public class Algus {
         if(temperatuur>0){System.out.println("sulab");}
         else{System.out.println("Jäätab");}    
     }
+    
     /**
      * Programmi töö alguskoht. Siit alustatakse käivitamisel
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
         System.out.println("Hello, Web!");
 
-        ilmaKommentaar(3);
-
+        ilmaKommentaar(-2);
+        toatemperatuur(9);
     }
+    
     
 }

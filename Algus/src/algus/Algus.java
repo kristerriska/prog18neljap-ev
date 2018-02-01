75041765efb95bf958eab0803b1244c2cef43f81
if17 <if17@A406-10.INFORMAATIKA>
2018-02-01 09:22:21 +0200

    uue projekti loomine

---
+++ Algus/src/algus/Algus.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algus;
+
+/**
+ *
+ * @author if17
+ */
+public class Algus {
+
+    /**
+     * Esimene katsetus. Muutujad, valik, massiiv
+     * Programmi töö alguskoht
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, Web");
+    }
+    
+}

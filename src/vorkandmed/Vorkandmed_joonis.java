95dd3c7bcbd1c9e407f8489a8c3c2cf86797b160
if17 <if17@A406-4.INFORMAATIKA>
2018-02-22 09:48:27 +0200

    Õunte Joonis

--- src/vorkandmed/Vorkandmed_joonis.java
+++ src/vorkandmed/Vorkandmed_joonis.java
@@ -1,16 +1,55 @@
 package vorkandmed;
-import java.net.*;
-import java.io.*;
+
 import javax.swing.*;
+import java.util.*;
 import java.awt.*;
-public class Vorkandmed_joonis extends JPanel{
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class Vorkandmed_joonis extends JPanel {
+
+    java.util.List<double[]> ounad;
+
+    public java.util.List<double[]> kysiOunteAndmed() {
+        if (ounad == null) {
+            ounad=new ArrayList<>();
+            try {
+                String aadress = "http://www.tlu.ee/~jaagup/andmed/muu/ounad/antoonovka2.txt";
+                BufferedReader br = new BufferedReader(new InputStreamReader(
+                        new URL(aadress).openStream()
+                ));
+                String rida = br.readLine();
+                rida = br.readLine();
+                while (rida != null) {
+                    String[] m = rida.split(",");
+                    double[] dm=new double[m.length];
+                    for (int i = 0; i < m.length; i++) {
+                        dm[i]=Double.parseDouble(m[i]);
+                    }
+                    ounad.add(dm);
+                    rida = br.readLine();
+                }
+                br.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        return ounad;
+    }
+
     @Override
-    public void paintComponent (Graphics g) {
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.fillOval(30, 40, 5, 5);
+        double koef=10;
+        for(double[] m: kysiOunteAndmed()){
+           g.fillOval(getHeight()-(int)(m[0]*10), (int)(m[1]*10), 5, 5);
+        }
     }
+
     public static void main(String[] arg) {
-        JFrame f=new JFrame ("Õunte joonis");
+        JFrame f = new JFrame("Õunte joonis");
         f.getContentPane().add(new Vorkandmed_joonis());
         f.setSize(400, 300);
         f.setVisible(true);
